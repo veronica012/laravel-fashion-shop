3f0fe256a59e68624b9e388a96b733923ec3f1c3
Veronica Celseti <veronicacelseti12@gmail.com>
2020-07-07 11:47:15 +0200

    yield

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,11 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>@yeld('page-title')</title>
+        <title>@yield('page-title')</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        @yeld('content')
+        @yield('content')
         <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
     </body>
 </html>
